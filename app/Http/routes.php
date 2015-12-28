6e62c08599d1021ef8d2d26ff4439115a68635c9
Matheus Gomes <zuza-cs@hotmail.com>
2015-12-28 21:29:33 -0200

    Criando rotas admin/categories e admin/products

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/categories', ['uses' => 'AdminCategoriesController@index']);
+Route::get('admin/products', ['uses' => 'AdminProductsController@index']);
